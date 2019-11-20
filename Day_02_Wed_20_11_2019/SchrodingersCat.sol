df76949ee9c18f8e7c6b48387ce43ba79cd8f4d0
prushinskaite <prushinskaite@gmail.com>
2019-11-20 15:10:44 +0100

    made use of mapping, event and error handling

--- Day_02_Wed_20_11_2019/SchrodingersCat.sol
+++ Day_02_Wed_20_11_2019/SchrodingersCat.sol
@@ -2,8 +2,12 @@ pragma solidity >=0.5.11;
 
 /*
 * This is the contract that imitates the Copenhagen interpretation of Schrodinger's Cat problem.
+* The key difference is that the observer pays to create a box and when the box is opened and cat
+* appears to be alive, observer is rewarded with her/his fee.
 **/
 contract SchrodingersCat {
+
+    event Observed(bool is_alive);
 
     enum CatState {Superposition, Alive, Dead}
     enum BoxState {Open, Closed}
@@ -13,32 +17,48 @@ contract SchrodingersCat {
         BoxState box_state;
     }
 
-    /**
-    * Initially the box is closed and the cat is in the superposition.
-    */
-    Box public myBox =  Box(CatState.Superposition, BoxState.Closed);
+    mapping(address => Box) public boxes;
 
     /**
-    * Checks whether the box is in the closed state.
+    * Create a box that is mapped to the sender address. Initially the box is closed and the cat is in the superposition.
     */
-    modifier boxIsClosed() {
-        require(myBox.box_state == BoxState.Closed, "Box has already been opened");
-        _;
+    function createBox() public {
+        boxes[msg.sender] = Box(CatState.Superposition, BoxState.Closed);
     }
 
     /**
     * Opens the box and observes the contents of the box.
     */
-    function openBox () public boxIsClosed {
+    function openBox () public payable boxIsClosed {
+        address payable observer = address(msg.sender);
+        uint fee = msg.value;
+        require(fee > 0, "Value must be higher than 0.");
+        Box memory myBox = boxes[msg.sender];
+        // what happens if map doesn't contain this key?
         myBox.box_state = BoxState.Open;
-        getRandom() ? myBox.cat_state = CatState.Alive : myBox.cat_state = CatState.Dead;
+        if(getRandom()) {
+            myBox.cat_state = CatState.Alive;
+            // if the cat is alive when the box is open -> give reward to observer
+            observer.transfer(fee);
+        } else{
+            myBox.cat_state = CatState.Dead;
+        }
+        emit Observed(catIsAlive());
+    }
+
+    /**
+    * Checks whether the box is in the closed state.
+    */
+    modifier boxIsClosed() {
+        require(boxes[msg.sender].box_state == BoxState.Closed, "Box has already been opened");
+        _;
     }
 
     /**
     * Returns True if the cat is alive, otherwise False.
     */
     function catIsAlive() public view returns(bool){
-        return myBox.cat_state == CatState.Alive;
+        return boxes[msg.sender].cat_state == CatState.Alive;
     }
 
     /**
