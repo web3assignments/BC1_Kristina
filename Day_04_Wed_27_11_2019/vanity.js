dd374c34afa2209b6ffd080ac8c51d6c7f2fac6e
prushinskaite <prushinskaite@gmail.com>
2019-12-04 09:42:10 +0100

    added results of running vanity script

--- Day_04_Wed_27_11_2019/vanity.js
+++ Day_04_Wed_27_11_2019/vanity.js
@@ -11,6 +11,6 @@ do {
 
 } while (prefix != find);
 console.log(`prefix = ${prefix}`)
-console.log(`address = ${newAddr.address}`)
-console.log(`pk = ${newAddr.privateKey}`)
-console.log(`nr of iterations: ${i}`) 
+console.log(`address = ${newAddr.address}`) //0x543211C5D0Fc20a01f74CD45912F921848C834A1
+console.log(`pk = ${newAddr.privateKey}`) //0x69dd363db009fc26f8b1dee4bfa47f17d91ecc6a1a8df3101344ca888950d255
+console.log(`nr of iterations: ${i}`) // 235,753
